cbda39053bc034b4b07b24522ec995ce94d8ae97
포스코DX-비트아카데미 <141541801+poscodx-bitacademy@users.noreply.github.com>
2023-09-11 08:34:58 +0900

    Add files via upload

--- mysite02/src/main/java/com/poscodx/mysite/controller/GuestbookController.java
+++ mysite02/src/main/java/com/poscodx/mysite/controller/GuestbookController.java
@@ -1,54 +1,29 @@
 package com.douzone.mysite.controller;
 
-import java.util.List;
+import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import com.douzone.mysite.service.GuestbookService;
-import com.douzone.mysite.vo.GuestbookVo;
+import com.douzone.mysite.web.mvc.guestbook.GuestbookActionFactory;
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
 
-@Controller
-@RequestMapping("/guestbook")
-public class GuestbookController {
-	@Autowired
-	private GuestbookService guestbookService;
-	
-	@RequestMapping("")
-	public String index(Model model) {
-		List<GuestbookVo> list = guestbookService.getMessageList();
-		model.addAttribute("list", list);
+
+public class GuestbookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String actionName = request.getParameter("a");
 		
-		return "guestbook/index";
-	}
-	
-	@RequestMapping(value="/add", method=RequestMethod.POST)
-	public String add(GuestbookVo vo) {
-		System.out.println(vo);
-		guestbookService.addMessage(vo);
-		System.out.println(vo);
-		return "redirect:/guestbook";
-	}
-	
-	@RequestMapping(value="/delete/{no}", method=RequestMethod.GET)
-	public String delete(@PathVariable("no") Long no, Model model) {
-		model.addAttribute("no", no);
-		return "guestbook/delete";
+		ActionFactory af = new GuestbookActionFactory(); 
+		Action action = af.getAction(actionName);
+		action.execute(request, response);
 	}
 
-	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST)
-	public String delete(@PathVariable("no") Long no, @RequestParam(value="password", required=true, defaultValue="") String password) {
-		guestbookService.deleteMessage(no, password);
-		return "redirect:/guestbook";
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
-	
-	@RequestMapping("/spa")
-	public String indexSPA() {
-		return "guestbook/index-spa";
-	}	
 }
